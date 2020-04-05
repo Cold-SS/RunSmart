736f0bda0c495e8e5fab023c3475671e524930af
Cold-SS <60955303+Cold-SS@users.noreply.github.com>
2020-04-05 15:25:03 +0700

    Added courusel section

---
+++ src/js/scripts.js
@@ -0,0 +1,19 @@
+var slider = tns({
+    container: '.carousel__slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    center: true,
+    controls: false,
+    speed: 900
+  });
+
+  document.querySelector('.carousel__prev-btn').addEventListener('click',
+    function () {
+        slider.goTo('prev');
+  });
+
+  document.querySelector('.carousel__next-btn').addEventListener('click',
+    function () {
+        slider.goTo('next');
+});
